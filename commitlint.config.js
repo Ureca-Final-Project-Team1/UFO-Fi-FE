b5b62440f4f3ddc0dc0c5c191c1d4f4ec48f789d
abyss-s <lh3729@naver.com>
2025-07-07 03:37:34 +0900

    fix: [UF-75] commitlint에서 머지 커밋과 UF- 형식 허용 #42

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,38 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "feat",
-        "fix",
-        "refactor",
-        "docs",
-        "test",
-        "chore",
-        "style",
-        "delete",
-        "move",
-        "update",
+        'feat',
+        'fix',
+        'refactor',
+        'docs',
+        'test',
+        'chore',
+        'style',
+        'delete',
+        'move',
+        'update',
+        'ci', // CI 관련
+        'build', // 빌드 관련
       ],
     ],
-    "subject-case": [0], // 한글 허용
+    'subject-case': [0], // 한글 허용
+    'header-max-length': [0], // 길이 제한 해제
   },
+
+  // 무시할 패턴
+  ignores: [
+    (message) => message.startsWith('Merge pull request'),
+    (message) => message.startsWith('Merge branch'),
+    (message) => message.includes('Merge remote-tracking branch'),
+    (message) => message.startsWith('UF-'),
+    (message) => message.startsWith('[UF-'),
+    (message) => /^UF-\d+/.test(message),
+    (message) => /^\[UF-\d+\]/.test(message),
+    (message) => message.startsWith('Revert'),
+    (message) => message.startsWith('Initial commit'),
+  ],
 };
