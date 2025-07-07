4b08ce3e0536b09f2ee35a0debe9b2cad3215d19
kuru2141 <kuru2141@naver.com>
2025-07-07 17:28:43 +0900

    feat: [UF-65] complict 해결 #26

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,38 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "feat",
-        "fix",
-        "refactor",
-        "docs",
-        "test",
-        "chore",
-        "style",
-        "delete",
-        "move",
-        "update",
+        'feat',
+        'fix',
+        'refactor',
+        'docs',
+        'test',
+        'chore',
+        'style',
+        'delete',
+        'move',
+        'update',
+        'ci', // CI 관련
+        'build', // 빌드 관련
       ],
     ],
-    "subject-case": [0], // 한글 허용
+    'subject-case': [0], // 한글 허용
+    'header-max-length': [0], // 길이 제한 해제
   },
+
+  // 무시할 패턴
+  ignores: [
+    (message) => message.startsWith('Merge pull request'), // PR 머지
+    (message) => message.startsWith('Merge branch'), // 브랜치 머지
+    (message) => message.includes('Merge remote-tracking branch'), // 원격 브랜치 머지
+    (message) => message.startsWith('UF-'), // UF-123: 제목
+    (message) => message.startsWith('[UF-'), // [UF-123] 제목
+    (message) => /^UF-\d+/.test(message), // UF-숫자
+    (message) => /^\[UF-\d+\]/.test(message), // [UF-숫자]
+    (message) => message.startsWith('Revert'), // 리버트 커밋
+    (message) => message.startsWith('Initial commit'), // 초기 커밋
+  ],
 };
