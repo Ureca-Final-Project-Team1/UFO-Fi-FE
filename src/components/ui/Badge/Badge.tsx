d3ba947d874d0fdf35c231ae8cd8e9965cc35686
Jin <kuru2141@naver.com>
2025-07-08 10:57:41 +0900

    feat: [UF-85] badge props 추가 및 Icon 컴포넌트 적용 #54

--- src/components/ui/Badge/Badge.tsx
+++ src/components/ui/Badge/Badge.tsx
@@ -1,14 +1,47 @@
 import { Slot } from '@radix-ui/react-slot';
-import Image from 'next/image';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
 import type { BadgeProps } from './Badge.types';
 import { badgeVariants } from './badgeVariants';
+import { Icon } from '../Icons';
 
-function Badge({ className, variant, state, asChild = false, children, ...props }: BadgeProps) {
+const stateInfo = {
+  selling: {
+    label: '판매중',
+    icon: null,
+    showDot: true,
+  },
+  sold: {
+    label: '판매완료',
+    icon: 'CircleCheck',
+    showDot: false,
+  },
+  timeout: {
+    label: '기간만료',
+    icon: 'Hourglass',
+    showDot: false,
+  },
+  reported: {
+    label: '블라인드',
+    icon: 'EyeOff',
+    showDot: false,
+  },
+} as const;
+
+function Badge({
+  className,
+  variant,
+  state,
+  showIcon = false,
+  icon = 'AlertCircle',
+  asChild = false,
+  children,
+  ...props
+}: BadgeProps) {
   const Comp = asChild ? Slot : 'span';
+  const status = state ? stateInfo[state] : null;
 
   const appliedClass = state
     ? badgeVariants({ state })
@@ -16,22 +49,19 @@ function Badge({ className, variant, state, asChild = false, children, ...props 
 
   return (
     <Comp data-slot="badge" className={cn(appliedClass, className, 'caption-12-bold')} {...props}>
-      {state ? (
-        <>
-          <Image src={`/icons/badge/${state}.svg`} alt={state} width={12} height={12} />
-          <span>
-            {
-              {
-                selling: '판매중',
-                sold: '판매완료',
-                timeout: '기간만료',
-                reported: '블라인드',
-              }[state]
-            }
-          </span>
-        </>
+      {status ? (
+        <div className="flex items-center gap-1.5">
+          {showIcon && status.showDot && (
+            <div className={`w-3.5 h-3.5 rounded-full bg-(--color-text-${state})`} />
+          )}
+          {showIcon && status.icon && <Icon name={status.icon} alt={state ?? ''} size="sm" />}
+          <span>{status.label}</span>
+        </div>
       ) : (
-        children
+        <div className="flex items-center gap-1.5">
+          {showIcon && <Icon src={icon} alt="custom-icon" size="sm" />}
+          <span>{children}</span>
+        </div>
       )}
     </Comp>
   );
--- src/components/ui/Badge/Badge.types.ts
+++ src/components/ui/Badge/Badge.types.ts
@@ -2,9 +2,13 @@ import type { VariantProps } from 'class-variance-authority';
 import type * as React from 'react';
 
 import type { badgeVariants } from './badgeVariants';
+import { IconType } from '../Icons';
 
 export interface BadgeProps
   extends React.ComponentProps<'span'>,
     VariantProps<typeof badgeVariants> {
   asChild?: boolean;
+  showIcon?: boolean;
+  icon?: IconType;
+  children?: React.ReactNode;
 }
