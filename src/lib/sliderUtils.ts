03456bd71df054ceaf193d18ef229c7e5b02bce3
minji-38 <ahn.minji38@gmail.com>
2025-07-09 21:51:38 +0900

    style: [UF-105] 에어비엔비 스타일 가이드 적용 #78

--- src/lib/sliderUtils.ts
+++ src/lib/sliderUtils.ts
@@ -4,6 +4,7 @@ export function getGbLabel(i: number, steps: number, unit = 'GB') {
   if (i === steps) return `${steps}${unit}`;
   return i.toString();
 }
+
 // 슬라이더 범위 텍스트 생성 유틸
 export function getRangeLabel([min, max]: number[], step = 500, maxIndex = 20) {
   if (min === 0 && max !== maxIndex) return `${max * step}원 이하`;
