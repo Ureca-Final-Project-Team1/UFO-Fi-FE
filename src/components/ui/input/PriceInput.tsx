c3ac96949892129afa25b0f0f2a65f210392722b
minji-38 <ahn.minji38@gmail.com>
2025-07-09 17:07:49 +0900

    style: [UF-94] 가격 input color 수정(global 정의 색상) #67

--- src/components/ui/input/PriceInput.tsx
+++ src/components/ui/input/PriceInput.tsx
@@ -29,7 +29,7 @@ export function PriceInput(props: CustomInputProps) {
       pattern="[0-9,]*"
       placeholder="금액 입력"
       className={cn(
-        'bg-[#001f9c] text-[#23d8ff] text-center font-semibold placeholder-[#23d8ff]',
+        'bg-[var(--color-badge-bg-dark)] text-[var(--color-text-selling)] text-center font-semibold placeholder-[var(--color-text-selling)]',
         props.className,
       )}
     />
