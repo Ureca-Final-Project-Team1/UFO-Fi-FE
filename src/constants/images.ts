195e68869bd843b3d7914e46056ccf474bb1c9d4
abyss-s <lh3729@naver.com>
2025-07-10 00:29:05 +0900

    feat: [UF-98] 온보딩 관련 이미지 경로 상수화 #74

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,4 +2,10 @@
 export const IMAGE_PATHS = {
   AL_SAD: '/images/alien-sad.png',
   AL_HAPPY: '/images/alien-happy.png',
+  AL_ONBOARDING: '/images/alien-onboarding.png',
+  NEXT_BTN_ONBOARDING: '/images/next-btn.png',
+  FIRE_BTN_ONBOARDING: '/images/fire-btn.png',
+  BG_ONBOARDING: '/images/background-onboarding.png',
+  BG_LOGIN: '/images/background-login.png',
+  BG_BASIC: '/images/background-basic.png',
 } as const;
