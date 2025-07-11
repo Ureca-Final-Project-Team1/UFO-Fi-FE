35959a18fdce9bbade8bc747c0d97539330cd2b7
kuru2141 <kuru2141@naver.com>
2025-07-11 13:58:30 +0900

    fix: [UF-126] Input index.ts 추가 #98

--- src/shared/ui/input/index.ts
+++ src/shared/ui/input/index.ts
@@ -1,9 +1,9 @@
-export * from './AccountInput';
-export * from './BankSelectInput';
-export * from './BasicInput';
-export * from './LabeledInput';
-export * from './PhoneInput';
-export * from './PriceInput';
-export * from './Input';
+export { AccountInput } from './AccountInput';
+export { BankSelectInput } from './BankSelectInput';
+export { BasicInput } from './BasicInput';
+export { LabeledInput } from './LabeledInput';
+export { PhoneInput } from './PhoneInput';
+export { PriceInput } from './PriceInput';
+export { Input } from './Input';
 export type { CustomInputProps } from './Input.types';
 export { inputVariants } from './inputVariants';
