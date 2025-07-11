c90deb88cf308f11334ecdea66f75945835dcfdc
minji-38 <ahn.minji38@gmail.com>
2025-07-11 11:30:33 +0900

    fix: [UF-112] input 폴더 삭제 #86

--- src/components/ui/input_temp/Input.types.ts
+++
@@ -1,7 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-
-export interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  variant?: 'default' | 'whiteBorder' | 'blueFill';
-  label?: string;
-  error?: string;
-}
