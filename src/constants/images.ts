981af1973fc4f44a96bc2d4de6eb858b59914605
dogeonkim1 <caddibo4@naver.com>
2025-07-12 13:12:33 +0900

    feat: [UF-131] 404not-found 이미지 추가 #104

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -8,6 +8,7 @@ export const IMAGE_PATHS = {
   BG_LOGIN: '/images/background-login.png',
   BG_BASIC: '/images/background-basic.png',
   BLACKHOLE_REAL: '/images/blackhole-real.png',
+  NOT_FOUND: '/images/not-found.png',
 
   KAKAO_LOGIN_BUTTON: '/images/kakao-login-button.png',
   UFO_LOGIN: '/images/ufo-login.png',
