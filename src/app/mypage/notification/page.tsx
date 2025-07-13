593a9f06a36b3c234c6b9eb63ecb09ecd32dbc39
kuru2141 <kuru2141@naver.com>
2025-07-13 18:54:35 +0900

    feat: [UF-152] 알림 설정 페이지 수정 #124

--- src/app/mypage/notification/page.tsx
+++ src/app/mypage/notification/page.tsx
@@ -11,20 +11,27 @@ const Page = () => {
     <div className="body-16-semibold w-full h-full flex flex-col items-center pt-4 gap-4 text-white">
       <Title title="알림 설정하기" className="body-20-bold" />
 
-      <div className="grid grid-cols-[80px_1fr_auto] gap-y-4 w-full max-w-md items-center">
-        <p className="col-span-2">거래 알림 전체</p>
-        <Switch className="col-start-3" />
+      <div className="w-full max-w-md flex flex-col gap-6">
+        <div className="grid grid-cols-[80px_1fr_auto] gap-y-4 items-center">
+          <p className="col-span-2">거래 알림</p>
+          <Switch className="col-start-3" />
 
-        <p className="text-gray-400">데이터</p>
-        <p className="ml-[20%]">데이터 판매 알림</p>
-        <Switch />
+          <p className="text-gray-400">데이터</p>
+          <p className="ml-[20%]">데이터 판매 알림</p>
+          <Switch />
 
-        <p className="col-start-2 ml-[20%]">관심 상품 알림</p>
-        <Switch />
+          <p className="col-start-2 ml-[20%]">관심 상품 알림</p>
+          <Switch />
+        </div>
 
-        <p className="text-gray-400">신고</p>
-        <p className="ml-[20%]">신고 누적 정지 알림</p>
-        <Switch />
+        <div className="grid grid-cols-[80px_1fr_auto] gap-y-4 items-center">
+          <p className="col-span-2">신고 알림</p>
+          <Switch className="col-start-3" />
+
+          <p className="text-gray-400">신고</p>
+          <p className="ml-[20%]">신고 누적 정지 알림</p>
+          <Switch />
+        </div>
       </div>
     </div>
   );
