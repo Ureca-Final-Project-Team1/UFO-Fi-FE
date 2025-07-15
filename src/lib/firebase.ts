bfa9ae1f8ccfea1b32d2d1a186d554507cb17673
abyss-s <lh3729@naver.com>
2025-07-16 01:31:59 +0900

    fix: [UF-171] 공개 가능한 환경변수는 공개처리 #142

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getMessaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
+  apiKey: 'AIzaSyCjsd6DSSvstY2FZxypONKJT2_k0OC4W_U',
+  authDomain: 'ufo-fi.firebaseapp.com',
+  projectId: 'ufo-fi',
+  storageBucket: 'ufo-fi.firebasestorage.app',
+  messagingSenderId: '176796950226',
+  appId: '1:176796950226:web:54a15f105410f70d49570b',
+  measurementId: 'G-CC7MQ86YEB',
 };
 
 // Firebase 앱 초기화
