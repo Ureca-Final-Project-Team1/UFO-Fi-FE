554228262cae787a5f19c14bcf992a1c41af138a
kuru2141 <kuru2141@naver.com>
2025-07-16 21:49:37 +0900

    feat: [UF-184] mypage notification API 추가 #155

--- src/app/mypage/notification/page.tsx
+++ src/app/mypage/notification/page.tsx
@@ -1,28 +1,74 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { getNotificationAPI } from '@/api/notification/getNotificationAPI';
+import { updateNotificationAPI } from '@/api/notification/updateNotificationAPI';
 import { Title } from '@/shared';
 import { Switch } from '@/shared/ui/Switch';
 import '@/styles/globals.css';
 
-const Page = () => {
-  const [tradeAll, setTradeAll] = useState(false);
-  const [tradeSell, setTradeSell] = useState(false);
-  const [tradeInterest, setTradeInterest] = useState(false);
+type NotificationKey =
+  | 'BENEFIT'
+  | 'SELL'
+  | 'INTERESTED_POST'
+  | 'FOLLOWER_POST'
+  | 'REPORTED'
+  | 'TRADE';
 
-  const [reportAll, setReportAll] = useState(false);
-  const [reportStop, setReportStop] = useState(false);
+const tradeKeys: NotificationKey[] = ['SELL', 'INTERESTED_POST', 'FOLLOWER_POST', 'REPORTED'];
 
-  const toggleTradeAll = (value: boolean) => {
-    setTradeAll(value);
-    setTradeSell(value);
-    setTradeInterest(value);
-  };
+const MypageNotificationPage = () => {
+  const [notificationState, setNotificationState] = useState({
+    BENEFIT: false,
+    SELL: false,
+    INTERESTED_POST: false,
+    FOLLOWER_POST: false,
+    REPORTED: false,
+    TRADE: false,
+  });
 
-  const toggleReportAll = (value: boolean) => {
-    setReportAll(value);
-    setReportStop(value);
+  useEffect(() => {
+    const fetchNotificationSettings = async () => {
+      try {
+        // TODO: 인증 도입 시 변경할 것
+        const response = await getNotificationAPI({ userId: 1 });
+        if (!response) return;
+
+        setNotificationState({
+          BENEFIT: response.benefit ?? false,
+          SELL: response.sell ?? false,
+          INTERESTED_POST: response.interestedPost ?? false,
+          FOLLOWER_POST: response.followerPost ?? false,
+          REPORTED: response.reported ?? false,
+          TRADE:
+            response.sell && response.interestedPost && response.followerPost && response.reported,
+        });
+      } catch (err) {
+        console.error('알림 설정 조회 실패:', err);
+      }
+    };
+
+    fetchNotificationSettings();
+  }, []);
+
+  const handleToggle = async (type: NotificationKey, value: boolean) => {
+    const newNotificationState = { ...notificationState, [type]: value };
+
+    // TODO: 인증 도입 시 변경할 것
+    updateNotificationAPI({ userId: 1, type, enable: value });
+
+    if (type === 'TRADE') {
+      tradeKeys.forEach((key) => {
+        newNotificationState[key] = value;
+      });
+    } else if (tradeKeys.includes(type)) {
+      newNotificationState.TRADE = false;
+    }
+    newNotificationState[type] = value;
+
+    setNotificationState(newNotificationState);
+    return;
   };
 
   return (
@@ -31,28 +77,52 @@ const Page = () => {
 
       <div className="w-full max-w-md flex flex-col gap-6">
         <div className="grid grid-cols-[80px_1fr_auto] gap-y-4 items-center">
-          <p className="col-span-2">거래 알림</p>
-          <Switch className="col-start-3" checked={tradeAll} onCheckedChange={toggleTradeAll} />
-
-          <p className="text-gray-400">데이터</p>
-          <p className="ml-[20%]">데이터 판매 알림</p>
-          <Switch checked={tradeSell} onCheckedChange={setTradeSell} />
-
-          <p className="col-start-2 ml-[20%]">관심 상품 알림</p>
-          <Switch checked={tradeInterest} onCheckedChange={setTradeInterest} />
+          <p className="col-span-2">이벤트 혜택 수신 알림</p>
+          <Switch
+            className="col-start-3"
+            checked={notificationState.BENEFIT}
+            onCheckedChange={(val) => handleToggle('BENEFIT', val)}
+          />
         </div>
 
         <div className="grid grid-cols-[80px_1fr_auto] gap-y-4 items-center">
-          <p className="col-span-2">신고 알림</p>
-          <Switch className="col-start-3" checked={reportAll} onCheckedChange={toggleReportAll} />
+          <p className="col-span-2">거래 알림 전체</p>
+          <Switch
+            className="col-start-3"
+            checked={notificationState.TRADE}
+            onCheckedChange={(val) => handleToggle('TRADE', val)}
+          />
+
+          <p className="text-gray-400">데이터</p>
+          <p className="ml-[20%]">데이터 판매 알림</p>
+          <Switch
+            checked={notificationState.SELL}
+            onCheckedChange={(val) => handleToggle('SELL', val)}
+          />
+
+          <p className="col-start-2 ml-[20%]">관심 상품 알림</p>
+          <Switch
+            checked={notificationState.INTERESTED_POST}
+            onCheckedChange={(val) => handleToggle('INTERESTED_POST', val)}
+          />
 
           <p className="text-gray-400">신고</p>
           <p className="ml-[20%]">신고 누적 정지 알림</p>
-          <Switch checked={reportStop} onCheckedChange={setReportStop} />
+          <Switch
+            checked={notificationState.REPORTED}
+            onCheckedChange={(val) => handleToggle('REPORTED', val)}
+          />
+
+          <p className="text-gray-400">팔로우</p>
+          <p className="ml-[20%]">새 상품 알림</p>
+          <Switch
+            checked={notificationState.FOLLOWER_POST}
+            onCheckedChange={(val) => handleToggle('FOLLOWER_POST', val)}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default Page;
+export default MypageNotificationPage;
