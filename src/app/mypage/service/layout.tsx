777f735944d534bb2cbaafdf21ee2bc53b83d810
dogeonkim1 <caddibo4@naver.com>
2025-07-16 16:51:22 +0900

    feat: [UF-182] 이용 약관 페이지 구현 #153

---
+++ src/app/mypage/service/layout.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '이용 약관',
+  description: '이용 약관 페이지',
+  robots: 'index, follow',
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="w-full min-h-full">{children}</div>;
+}
