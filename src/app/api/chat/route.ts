e1792af886e5bde95660aaafecd222cfff514f70
kuru2141 <kuru2141@naver.com>
2025-07-18 20:19:16 +0900

    feat: [UF-188] 환경변수 이름 변경 #164

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_SECRET_KEY!,
+  apiKey: process.env.OPENAI_API_KEY!,
   organization: process.env.OPENAI_ORGANIZATION_ID!,
 });
 
