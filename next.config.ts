47dfce48af83892c451e9a863d5ca569dc4fdd50
abyss-s <lh3729@naver.com>
2025-07-20 05:03:10 +0900

    feat: [UF-200] S3 버킷 이미지 서버 도메인 사용 설정을 위한 환경변수 추가 #183

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_IMAGE_DOMAIN || '',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.md$/,
