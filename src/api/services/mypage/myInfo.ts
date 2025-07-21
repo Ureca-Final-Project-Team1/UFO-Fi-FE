c349c18bf73e6e3e381770d66bdeb32b2c6f0b50
minji-38 <ahn.minji38@gmail.com>
2025-07-21 13:47:44 +0900

    fix: [UF-193] withCredentials 중복 제거 #171

--- src/api/services/mypage/myInfo.ts
+++ src/api/services/mypage/myInfo.ts
@@ -4,12 +4,7 @@ import { MyInfoResponse } from '@/api/types/myInfo';
 export const myInfoAPI = {
   async get(): Promise<MyInfoResponse['content'] | undefined> {
     try {
-      const response = await apiRequest.get<{ content: MyInfoResponse['content'] }>('/v1/mypage', {
-        withCredentials: true,
-        headers: {
-          Accept: '*/*',
-        },
-      });
+      const response = await apiRequest.get<{ content: MyInfoResponse['content'] }>('/v1/mypage');
       return response.data.content;
     } catch (error) {
       console.error('마이페이지 나의 프로필 조회 실패:', error);
