66eb6ad7c027fef122c265370d1d05fd55545701
dogeonkim1 <caddibo4@naver.com>
2025-07-21 13:32:17 +0900

    refactor: [UF-216] 블랙홀 페이지의 요소 위치 재배치 #205

--- src/app/blackhole/page.tsx
+++ src/app/blackhole/page.tsx
@@ -9,7 +9,7 @@ export default function BlackholePage() {
   return (
     <div className="flex flex-col items-center min-h-full relative w-full">
       <Title title="홈" iconVariant="back" className="mb-0" />
-      <div className="flex-1 flex flex-col items-center justify-center w-full">
+      <div className="flex flex-col items-center w-full mt-25">
         <p className="body-16-bold text-white-600 text-center mb-2">
           당신은 안드로메다로 들어갔습니다...<span className="ml-1">😢</span>
         </p>
