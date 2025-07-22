28355cc0f8341612e40cd52fcbdf2fe092128deb
minji-38 <ahn.minji38@gmail.com>
2025-07-22 22:04:18 +0900

    refactor: [UF-208] 확인 모달 위치 마이페이지 컴포넌트 폴더로 위치 조정을 위해 삭제 #195

--- src/shared/ui/Modal/index.ts
+++ src/shared/ui/Modal/index.ts
@@ -2,4 +2,3 @@ export { Modal } from './Modal';
 export { GlobalModal } from './GlobalModal';
 export { modalVariants, modalOverlayVariants } from './modalVariants';
 export type { ModalProps } from './Modal.types';
-export { ConfirmModal } from './ConfirmModal';
