22eaee23ee8267b600f7116cecfbb3b3cbe674f7
minji-38 <ahn.minji38@gmail.com>
2025-07-22 22:19:33 +0900

    feat: [UF-208] 닉네임, 요금제 변경 로직 분리 전 각각 컴포넌트화 #195

--- src/features/mypage/components/index.ts
+++ src/features/mypage/components/index.ts
@@ -2,3 +2,6 @@ export { TradeHistoryCard } from './TradeHistoryCard';
 export { type TradeHistoryCardProps } from '../types/TradeHistoryCard.types';
 export * from './SignalCard';
 export * from './MenuSection';
+export { ConfirmModal } from './ConfirmModal';
+export { NicknameEditor } from './NicknameEditor';
+export { PlanEditor } from './PlanEditor';
