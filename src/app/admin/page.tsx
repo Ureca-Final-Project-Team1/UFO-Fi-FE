41777243515b14d3256ff373a86b236575cd97c4
dogeonkim1 <caddibo4@naver.com>
2025-07-22 17:43:24 +0900

    feat: [UF-225] 대쉬보드 헤더, 사이드바 1차 구현 #216

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,18 +7,20 @@ import Sidebar from '@/shared/ui/Sidebar/Sidebar';
 
 export default function AdminPage() {
   return (
-    <div className="flex min-h-screen w-full bg-gray-100">
-      {/* 사이드바 */}
-      <Sidebar />
-      {/* 메인 컨텐츠 */}
-      <main className="flex-1 flex flex-col">
-        {/* 헤더 */}
-        <Header userName="Admin" />
-        {/* 컨텐츠 영역 */}
-        <section className="flex-1 p-8">
-          <div className="text-gray-700 text-lg">여기에 어드민 기능별 컨텐츠가 들어갑니다.</div>
-        </section>
-      </main>
-    </div>
+    <>
+      {/* 헤더 */}
+      <Header userName="Admin" />
+      <div className="flex min-h-screen w-full bg-gray-100">
+        {/* 사이드바 */}
+        <Sidebar />
+        {/* 메인 컨텐츠 */}
+        <main className="flex-1 flex flex-col">
+          {/* 컨텐츠 영역 */}
+          <section className="flex-1 p-8">
+            <div className="text-gray-700 text-lg">여기에 어드민 기능별 컨텐츠가 들어갑니다.</div>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
