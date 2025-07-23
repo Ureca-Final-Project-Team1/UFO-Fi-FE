71a1d86c110c140957d68d8c58d8d24e31bbb503
dogeonkim1 <caddibo4@naver.com>
2025-07-23 10:37:16 +0900

    fix: [UF-225] import 구문 수정 #216

--- src/shared/ui/Table/Table.stories.tsx
+++ src/shared/ui/Table/Table.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Table from './Table';
+import { Table } from './Table';
 import { Icon } from '../Icons/Icon';
 
 const meta: Meta<typeof Table> = {
