ec9573ebd945c4cccf901f21c3b46d11fe7e80f2
kuru2141 <kuru2141@naver.com>
2025-07-25 14:57:52 +0900

    feat: [UF-260] 업적 페이지 제작 #268

---
+++ src/app/mypage/achievement/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import Image from 'next/image';
+
+import { AchievementBadge } from '@/features/mypage/components/AchievementBadge';
+import { Title } from '@/shared';
+import { useViewportStore } from '@/stores/useViewportStore';
+
+export default function AchievementPage() {
+  const { isDesktop, isTablet } = useViewportStore();
+
+  return (
+    <div className="relative w-full min-h-screen items-center flex flex-col gap-6">
+      <Title iconVariant="back" className="w-full body-20-bold pb-2" title="업적 도감" />
+      <p className="w-full text-center caption-12-medium">
+        작지만 즐거운 업적을 모두 달성하고
+        <br />
+        칭호를 받아보세요!
+      </p>
+      <Image
+        src="/images/alien-pointing.svg"
+        alt="pointer"
+        width={isDesktop ? 100 : isTablet ? 80 : 60}
+        height={isDesktop ? 100 : isTablet ? 80 : 60}
+        className="absolute top-[20px] right-[16px] z-20"
+      />
+
+      {[1, 2, 3, 4].map((i) => (
+        <div key={i} className="flex flex-col gap-4 w-full">
+          <hr className="border-t border-(--color-hr-border) w-full" />
+          <p className="body-16-bold text-white">Lv.{i}</p>
+          <div className="relative w-full flex items-end justify-evenly">
+            <Image src="/images/cloud.svg" alt="cloud" fill className="z-0" />
+            {[1, 2, 3].map((j) => (
+              <div key={`${i}-${j}`} className="relative z-10 flex flex-col items-center gap-1">
+                <AchievementBadge i={i} j={j} isAchieve={false} />
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/features/mypage/components/AchievementBadge.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+
+export interface AchievementBadgeProps {
+  className?: string;
+  i: number;
+  j: number;
+  isAchieve: boolean;
+}
+
+export const AchievementBadge = ({ className, i, j, isAchieve }: AchievementBadgeProps) => {
+  return (
+    <div key={j} className={`${className} flex flex-col mb-5`}>
+      <div className="relative w-[70px] h-[70px] border-2 border-(--color-badge-border) rounded-[14px] object-cover">
+        <Image
+          key={`lv${i}-${j}`}
+          src={`/icons/badges/lv${i}-${j}.svg`}
+          alt={`${i}-${j}`}
+          fill
+          className={`${!isAchieve ? 'blur' : ''}`}
+        />
+        {!isAchieve && (
+          <div className="w-[70px] h-[70px] flex justify-center items-center">
+            <Image
+              src="/icons/badges/locked.svg"
+              alt="locked"
+              width={35}
+              height={35}
+              className="absolute z-10"
+            />
+          </div>
+        )}
+      </div>
+      <p className="text-center pt-12 caption-14-regular">업적 이름</p>
+    </div>
+  );
+};
