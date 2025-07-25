8eac761d9e0f4cd72af9ed0a77e3f6f4403b7622
dogeonkim1 <caddibo4@naver.com>
2025-07-25 14:34:19 +0900

    fix: [UF-249] alias를 사용해 코드 가독성 향상 #256

--- src/shared/ui/Title/index.ts
+++ src/shared/ui/Title/index.ts
@@ -1,3 +1,3 @@
 export { Title } from './Title';
-export { TitleWithRouter } from '../../../features/common/components/TitleWithRouter';
+export { TitleWithRouter } from '@/features/common/components/TitleWithRouter';
 export type { TitleProps, TitleIconVariant } from './Title.types';
