2c58fa834b6ca2702a9bee3f4a08d450ec54967e
minji-38 <ahn.minji38@gmail.com>
2025-07-27 19:04:01 +0900

    refactor: [UF-289] 말풍선 궤도 안에서 호출하도록 변경 #312

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { SpeechBubble } from '@/features/main/components';
 import OrbitWithSatellite from '@/features/main/components/OrbitWithSatellite';
 import PlanetProgressBar from '@/features/main/components/PlanetProgressBar';
 
@@ -6,9 +5,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-row justify-center w-full min-h-full">
       <div className="relative w-full h-screen bg-[url(/bg.png)] bg-cover">
-        {/* 말풍선 */}
-        <SpeechBubble tailDirection="bottom">지구인님, 오늘 거래하실 건가요?</SpeechBubble>
-        {/* 궤도 + 위성 (클라이언트 컴포넌트) */}
+        {/* 궤도 + 위성 + 말풍선 */}
         <OrbitWithSatellite />
 
         <div className="absolute bottom-10 left-1/2 -translate-x-1/2">
