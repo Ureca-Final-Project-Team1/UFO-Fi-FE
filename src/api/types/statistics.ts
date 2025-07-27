4f23b2be2c20df3f890773580083406fab189d5c
minji-38 <ahn.minji38@gmail.com>
2025-07-27 22:15:07 +0900

    feat: [UF-278] 비활성화 통계 데이터 타입 추가 #302

--- src/api/types/statistics.ts
+++ src/api/types/statistics.ts
@@ -6,9 +6,24 @@ export interface StatisticsData {
   allReportCount: number;
 }
 
+// 비활성화 통계 데이터 타입
+export interface ReportsStatisticsData {
+  allUsersCount: number;
+  reportedUsersCount: number;
+  pendingReportPostCount: number;
+  confirmedReportPostCount: number;
+}
+
 // 통계 API 응답 타입
 export interface StatisticsResponse {
   statusCode: number;
   message: string;
   content: StatisticsData;
+}
+
+// 비활성화 통계 API 응답 타입
+export interface ReportsStatisticsResponse {
+  statusCode: number;
+  message: string;
+  content: ReportsStatisticsData;
 }
