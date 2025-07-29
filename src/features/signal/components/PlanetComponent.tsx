6bfc3d65e78483970ab2d64d2f5c5aef1658b20b
minji-38 <ahn.minji38@gmail.com>
2025-07-30 00:44:15 +0900

    feat: [UF-310] 활성화 된 행성인 경우 컬러+ 위성, 활성화 된 행성 사이 점선 컬러처리 #347

--- src/features/signal/components/PlanetComponent.tsx
+++ src/features/signal/components/PlanetComponent.tsx
@@ -1,30 +1,42 @@
 import Image from 'next/image';
 import React from 'react';
 
-interface Props {
+interface PlanetComponentProps {
   planetSrc: string;
   satelliteSrc: string;
-  message?: string;
-  planetSize?: number;
+  planetSize: number;
+  isArrived: boolean;
 }
 
-const PlanetComponent = React.forwardRef<HTMLDivElement, Props>(
-  ({ planetSrc, satelliteSrc, message, planetSize }, ref) => {
+const PlanetComponent = React.forwardRef<HTMLDivElement, PlanetComponentProps>(
+  ({ planetSrc, satelliteSrc, planetSize, isArrived }, ref) => {
     return (
       <div
         ref={ref}
         className="relative flex flex-col items-center z-10"
         style={{ minWidth: planetSize, height: planetSize }}
       >
+        {/* 도착한 경우에만 위성 표시 */}
+        {isArrived && (
+          <Image
+            src={satelliteSrc}
+            alt="위성"
+            width={30}
+            height={30}
+            className="absolute top-[-12px]"
+          />
+        )}
+
+        {/* 행성 이미지 - 도착 여부에 따라 흑백/컬러 처리 */}
         <Image
-          src={satelliteSrc}
-          alt="위성"
-          width={30}
-          height={30}
-          className="absolute top-[-12px]"
+          src={planetSrc}
+          alt="행성"
+          width={planetSize}
+          height={planetSize}
+          className={!isArrived ? 'grayscale' : ''}
         />
-        <Image src={planetSrc} alt="행성" width={planetSize} height={planetSize} />
-        {message && <span className="absolute top-[-36px] text-sm text-white">{message}</span>}
+
+        {isArrived && <span className="absolute top-[-36px] text-sm text-white">도착!</span>}
       </div>
     );
   },
