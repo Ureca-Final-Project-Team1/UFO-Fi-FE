538f94acc4cc832e9a252c88c70b546f74672298
abyss-s <lh3729@naver.com>
2025-08-02 18:57:09 +0900

    fix: [UF-374] 터보팩으로 인한 md 파일 파싱 문제를 ts 파일로 변환해 해결 #462

--- src/app/mypage/privacy/page.tsx
+++ src/app/mypage/privacy/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import privacyRaw from '@/constants/terms/privacy.md?raw';
+import { privacyMarkdown } from '@/constants/terms/privacy';
 import { renderTermsWithHeadingsAndLinks } from '@/constants/terms/termsRenderer';
 import { Title } from '@/shared';
 
@@ -10,19 +10,9 @@ export default function TermsPage() {
   return (
     <div>
       <Title title="개인정보 처리방침" iconVariant="back" />
-      <div className="text-white overflow-y-auto text-sm max-h-[80vh] p-2 rounded-lg flex flex-col gap-1 leading-relaxed hide-scrollbar">
-        {renderTermsWithHeadingsAndLinks(privacyRaw)}
+      <div className="flex flex-col gap-1 leading-relaxed hide-scrollbar">
+        {renderTermsWithHeadingsAndLinks(privacyMarkdown)}
       </div>
-
-      <style jsx>{`
-        .hide-scrollbar {
-          -ms-overflow-style: none; /* IE and Edge */
-          scrollbar-width: none; /* Firefox */
-        }
-        .hide-scrollbar::-webkit-scrollbar {
-          display: none; /* Chrome, Safari, Opera */
-        }
-      `}</style>
     </div>
   );
 }
--- src/app/mypage/service/page.tsx
+++ src/app/mypage/service/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import serviceRaw from '@/constants/terms/service.md?raw';
+import { serviceMarkdown } from '@/constants/terms/service';
 import { renderTermsWithHeadingsAndLinks } from '@/constants/terms/termsRenderer';
 import { Title } from '@/shared';
 
@@ -10,18 +10,9 @@ export default function TermsPage() {
   return (
     <div>
       <Title title="이용약관" iconVariant="back" />
-      <div className="text-white overflow-y-auto text-sm max-h-[80vh] p-2 rounded-lg flex flex-col gap-1 leading-relaxed hide-scrollbar">
-        {renderTermsWithHeadingsAndLinks(serviceRaw)}
+      <div className="flex flex-col gap-1 leading-relaxed hide-scrollbar">
+        {renderTermsWithHeadingsAndLinks(serviceMarkdown)}
       </div>
-      <style jsx>{`
-        .hide-scrollbar {
-          -ms-overflow-style: none;
-          scrollbar-width: none;
-        }
-        .hide-scrollbar::-webkit-scrollbar {
-          display: none;
-        }
-      `}</style>
     </div>
   );
 }
