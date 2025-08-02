c81abe735ff48e281e44db4a1c8b4dbb75c62646
abyss-s <lh3729@naver.com>
2025-08-03 01:31:19 +0900

    refactor: [UF-380] 정렬 칩 주석 처리 및 코인 이미지 변경 #469

--- src/features/exchange/components/ExchangeFilters.tsx
+++ src/features/exchange/components/ExchangeFilters.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 
-import { Button, Chip, Icon } from '@/shared';
+import { Button, Icon } from '@/shared';
 
 export const ExchangeFilters = () => {
   const router = useRouter();
@@ -20,16 +20,12 @@ export const ExchangeFilters = () => {
         거래소 필터 및 정렬 옵션
       </h2>
 
-      {/* TODO: 추후 정렬 필요 */}
-
-      {/* 필터 칩들 */}
-      <div className="flex flex-wrap gap-2" role="group" aria-label="거래 게시물 필터">
-        <Chip rightIcon={<Icon name="ChevronDown" />} aria-label="통신사별 필터">
-          통신사
-        </Chip>
+      {/* TODO: 필터 칩들 */}
+      {/* <div className="flex flex-wrap gap-2" role="group" aria-label="거래 게시물 필터">
+        <Chip aria-label="통신사별 필터">통신사</Chip>
         <Chip aria-label="데이터 용량별 필터">용량</Chip>
         <Chip aria-label="가격별 필터">가격</Chip>
-      </div>
+      </div> */}
 
       {/* 일괄구매 버튼 */}
       <div className="ml-auto sm:absolute sm:right-0 sm:top-0">
