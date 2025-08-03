e64d7c0faefe71cf39fbe3198cc2e92cd6242fa6
minji-38 <ahn.minji38@gmail.com>
2025-08-03 16:33:10 +0900

    fix: [UF-388] metadata title 수정 #483

--- src/app/signal/layout.tsx
+++ src/app/signal/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'UFO-Fi | 전파궤도',
+  title: 'UFO-Fi | 전파 거리',
   description: 'UFO-Fi의 전파 거리 탐색 페이지',
   robots: 'index, follow',
 };
