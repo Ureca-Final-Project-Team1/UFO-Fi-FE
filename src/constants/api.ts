c2d3421b47920ce343d2c8e2e84328af8642c9eb
abyss-s <lh3729@naver.com>
2025-08-03 23:41:24 +0900

    update: [UF-397] API_ENDPOINTS pathname v2 업데이트 #498

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -45,5 +45,88 @@ export const COOKIE_CONFIG = {
 
 // API 엔드포인트
 export const API_ENDPOINTS = {
+  // User(6)
+  USER: {
+    SIGNUP_USER_INFO: '/signup/user-info',
+    PROFILE: (userId: number) => `/${userId}/profile`,
+    PROFILE_UPDATE: (userId: number) => `/${userId}/profile`,
+    ANOTHER_PROFILE: (anotherUserId: number) => `/${anotherUserId}/profile`,
+    ROLE: (userId: number) => `/${userId}/role`,
+    REPORTED_USERS: '/users/role/reported',
+  },
+
+  // UserPlan(3)
+  USER_PLAN: {
+    SIGNUP: '/user-plan',
+    GET_MY_PLAN: '/user-plan',
+    UPDATE_GET_MY_PLAN: (userPlanId: number) => `/user-plan/${userPlanId}`,
+  },
+
+  // Plan(1)
+  PLANS: '/plans',
+
+  // TradePost(5)
+  TRADE_POST: {
+    UPDATE: (postId: number) => `/posts/${postId}`,
+    DELETE: (postId: number) => `/posts/${postId}`,
+    LIST: '/posts', // 전체조회
+    CREATE: '/posts',
+    BULK_PURCHASE_GET: '/posts/bulk-purchase', // 일괄구매 조회
+  },
+
+  // Order(5)
+  ORDER: {
+    SALE_HISTORIES: '/trade-histories/sale', // 판매내역 조회
+    PURCHASE_HISTORIES: '/trade-histories/purchase', // 구매내역 조회
+    PURCHASE_DETAIL: (tradeHistoryId: number) => `/trade-histories/${tradeHistoryId}`, // 내역 상세보기
+    PURCHASE: '/posts/purchase', // 구매
+    BULK_PURCHASE_POST: '/posts/bulk-purchase', // 일괄구매 요청
+  },
+
+  // bannedword(4)
+  BANNED_WORDS: {
+    LIST: '/banned-words',
+    CREATE: '/banned-word',
+    DELETE_BULK: '/banned-words',
+    DELETE_SINGLE: (bannedWordId: number) => `/banned-words/${bannedWordId}`,
+  },
+
+  // Follow(4)
+  FOLLOW: {
+    FOLLOW: (followId: number) => `/follow/${followId}`,
+    UNFOLLOW: (followingId: number) => `/follow/${followingId}`,
+    FOLLOWINGS: '/follows/followings',
+    FOLLOWERS: '/follows/followers',
+  },
+
+  // Notification(4)
+  NOTIFICATION: {
+    FCM_TOKEN: '/fcm/token',
+    INTERESTED_POST_FILTER: '/notification-filters/interested-post',
+    SETTINGS_GET: '/notification-settings',
+    SETTINGS_UPDATE: '/notification-settings',
+  },
+
+  // InterestedPost
+
+  // Payment(2)
+  PAYMENT: {
+    STATUS: '/payment/status', // 결제 정보 검증 및 승인
+    CHARGE: '/payment', // 충전
+  },
+
+  // Report(2)
+  REPORT: {
+    ROLL_BACK: '/reports/report', // 어드민 신고 해지
+    REPORT_POST: (tradePostId: number) => `/trade-posts/${tradePostId}/report`, // 신고하기
+  },
+
+  // Statistics(2)
+  STATISTICS: {
+    BASIC: '/statistics',
+    REPORTS: '/statistics/reports',
+  },
+
   REFRESH: '/refresh',
+  LOGOUT: '/logout',
 } as const;
