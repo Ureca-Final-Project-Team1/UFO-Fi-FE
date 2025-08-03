d3984b9904eee1eedcf794d5facb829ba780ad97
kuru2141 <kuru2141@naver.com>
2025-08-04 01:51:35 +0900

    refactor: [UF-401] buil 페이지 css 수정 #508

--- src/app/exchange/bulk/page.tsx
+++ src/app/exchange/bulk/page.tsx
@@ -89,12 +89,19 @@ export default function BulkPurchasePage() {
           </div>
 
           {/* 외계인 캐릭터 */}
-          <div className="absolute bottom-[-90px] h-full right-0" aria-hidden="true">
+          <div
+            className="absolute h-full right-0"
+            aria-hidden="true"
+            style={{
+              right: isMobile ? '-30px' : '-50px',
+              bottom: isMobile ? '-100px' : '-150px',
+            }}
+          >
             <Image
               src={IMAGE_PATHS.AL_BULK_PURCHASE}
               alt=""
-              width={isMobile ? 200 : 260}
-              height={isMobile ? 200 : 260}
+              width={isMobile ? 200 : 300}
+              height={isMobile ? 200 : 300}
               priority
             />
           </div>
