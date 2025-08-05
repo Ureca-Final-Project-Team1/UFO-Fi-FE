669819eb1d13e0bd4aa825d5454ba3f326022b58
dogeonkim1 <caddibo4@naver.com>
2025-08-05 21:53:20 +0900

    refactor: [UF-416] Select 컴포넌트 타입들을 interface로 변환 #524

--- src/shared/ui/Select/Select.types.ts
+++ src/shared/ui/Select/Select.types.ts
@@ -19,28 +19,30 @@ export interface SelectTriggerProps
   size?: 'sm' | 'default';
 }
 
-export type SelectContentProps = React.ComponentProps<typeof SelectPrimitive.Content> &
-  VariantProps<typeof selectContentVariants>;
+export interface SelectContentProps
+  extends React.ComponentProps<typeof SelectPrimitive.Content>,
+    VariantProps<typeof selectContentVariants> {}
 
 export type SelectGroupProps = React.ComponentProps<typeof SelectPrimitive.Group>;
 
 export type SelectValueProps = React.ComponentProps<typeof SelectPrimitive.Value>;
 
-export type SelectItemProps = React.ComponentProps<typeof SelectPrimitive.Item> &
-  VariantProps<typeof selectItemVariants>;
+export interface SelectItemProps
+  extends React.ComponentProps<typeof SelectPrimitive.Item>,
+    VariantProps<typeof selectItemVariants> {}
 
-export type SelectLabelProps = React.ComponentProps<typeof SelectPrimitive.Label> &
-  VariantProps<typeof selectLabelVariants>;
+export interface SelectLabelProps
+  extends React.ComponentProps<typeof SelectPrimitive.Label>,
+    VariantProps<typeof selectLabelVariants> {}
 
-export type SelectSeparatorProps = React.ComponentProps<typeof SelectPrimitive.Separator> &
-  VariantProps<typeof selectSeparatorVariants>;
+export interface SelectSeparatorProps
+  extends React.ComponentProps<typeof SelectPrimitive.Separator>,
+    VariantProps<typeof selectSeparatorVariants> {}
 
-export type SelectScrollUpButtonProps = React.ComponentProps<
-  typeof SelectPrimitive.ScrollUpButton
-> &
-  VariantProps<typeof selectScrollButtonVariants>;
+export interface SelectScrollUpButtonProps
+  extends React.ComponentProps<typeof SelectPrimitive.ScrollUpButton>,
+    VariantProps<typeof selectScrollButtonVariants> {}
 
-export type SelectScrollDownButtonProps = React.ComponentProps<
-  typeof SelectPrimitive.ScrollDownButton
-> &
-  VariantProps<typeof selectScrollButtonVariants>;
+export interface SelectScrollDownButtonProps
+  extends React.ComponentProps<typeof SelectPrimitive.ScrollDownButton>,
+    VariantProps<typeof selectScrollButtonVariants> {}
