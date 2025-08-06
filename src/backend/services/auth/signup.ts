106d52568b8e7dee05317ee43702fd27891efd29
kuru2141 <kuru2141@naver.com>
2025-08-06 13:26:24 +0900

    refactor: [UF-397] signup post -> patch로 변경 #543

--- src/backend/services/auth/signup.ts
+++ src/backend/services/auth/signup.ts
@@ -4,7 +4,7 @@ import { API_ENDPOINTS } from '@/constants';
 
 export const signupAPI = {
   async signup(data: SignupRequest): Promise<SignupResponse> {
-    const response = await apiRequest.post<SignupResponse>(
+    const response = await apiRequest.patch<SignupResponse>(
       API_ENDPOINTS.USER.SIGNUP_USER_INFO,
       data,
     );
--- src/backend/services/auth/userInfo.ts
+++ src/backend/services/auth/userInfo.ts
@@ -4,7 +4,7 @@ import { API_ENDPOINTS } from '@/constants';
 
 export const getUserInfoAPI = {
   async getInfo(): Promise<getUserInfoResponse> {
-    const response = await apiRequest.get<getUserInfoResponse>(API_ENDPOINTS.USER.SIGNUP_USER_INFO);
+    const response = await apiRequest.get<getUserInfoResponse>(API_ENDPOINTS.USER.PROFILE);
     return response.data;
   },
 };
