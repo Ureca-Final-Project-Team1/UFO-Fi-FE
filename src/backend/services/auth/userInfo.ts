b2073dc583634b709ca6db583a4c9ef0fd1c2cf1
kuru2141 <kuru2141@naver.com>
2025-08-06 13:41:34 +0900

    refactor: [UF-397] 엔드포인트 수정 #543

--- src/backend/services/auth/userInfo.ts
+++ src/backend/services/auth/userInfo.ts
@@ -4,7 +4,7 @@ import { API_ENDPOINTS } from '@/constants';
 
 export const getUserInfoAPI = {
   async getInfo(): Promise<getUserInfoResponse> {
-    const response = await apiRequest.get<getUserInfoResponse>(API_ENDPOINTS.USER.PROFILE);
+    const response = await apiRequest.get<getUserInfoResponse>(API_ENDPOINTS.USER.SIGNUP_USER_INFO);
     return response.data;
   },
 };
